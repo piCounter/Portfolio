021af888dd54dd5df3c03b3040002ddaf9da2a76
piCounter <mike.docs2@gmail.com>
2021-02-01 11:35:19 -0500

    init repo for school projects

---
+++ STCC/CSC111_Java/src/Welcome.java
@@ -0,0 +1,12 @@
+
+public class Welcome 
+{
+	public static void main(String[] args) 
+	{
+		// Display message Hello World! on the console
+		System.out.println("Hello World!");  // String
+	    System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");  // String
+	    System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+	    // finished at page 19
+    }
+}
